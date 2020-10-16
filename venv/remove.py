3082d4ea8f3bf08df4d88ab2bf4bfa6d647e8d29
YellowWH <yellowwh@hotmail.com>
2020-10-16 19:11:37 +0900

    1016 文件删除 要转移

---
+++ venv/remove.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python2.7
+# coding=utf-8
+'''
+Created on 2020/10/15
+
+@author: YellowWH
+@Reference: wotiger
+'''
+import os
+import time
+import sys
+import sys, getopt
+
+
+def remove_old_files(path, during=7*86400):
+    now = time.time()
+
+    for f in os.listdir(path):
+        f = os.path.join(path, f)
+        if os.stat(f).st_mtime < now - during:
+           if os.path.isfile(f):
+              print "remove file:" + f
+              os.remove(f)
+
+def main(argv):
+    target_dir = ''
+    during = 240
+    try:
+        opts, args = getopt.getopt(argv,"ht:d:",["target=","during="])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            usage()
+            sys.exit()
+        elif opt in ("-t", "--target"):
+            target_dir = arg
+        elif opt in ("-d", "--during"):
+            during = int(arg)
+
+    if target_dir == '':
+        usage()
+        sys.exit()
+    else:
+        print "remove files in folder", target_dir, during
+        remove_old_files(target_dir, during)
+
+def usage():
+    print 'Usage:remove_old_files.py -t <target_dir> -d <during_time> '
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        usage()
+    else:
+        main(sys.argv[1:])
